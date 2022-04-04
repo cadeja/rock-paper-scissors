fe85988f796c9bcc302cfb4f05e552ed2c821a0b
Cade <cjayenn@gmail.com>
2022-04-04 16:54:25 -0400

    Finished bare bones game

--- script.js
+++ script.js
@@ -16,25 +16,29 @@ function computerPlay(){
     }
 }
 
-function playRound(playerSelection, computerSelection){
+function playMatch(playerSelection, computerSelection){
 
     playerSelection =  playerSelection.substring(0,1).toUpperCase() + 
             playerSelection.substring(1,).toLowerCase();
     
     if (playerSelection == computerSelection){
-        return "tie";
+        return "You Tied!";
     }
     else if ((playerSelection == "Rock" && computerSelection == "Scissors") ||
     (playerSelection == "Scissors" && computerSelection == "Paper") ||
     (playerSelection == "Paper" && computerSelection == "Rock")) {
-        return "win";
+        return `You Win! ${playerSelection} beats ${computerSelection}.`;
     }
     else if ((playerSelection == "Rock" && computerSelection == "Paper") ||
     (playerSelection == "Scissors" && computerSelection == "Rock") ||
     (playerSelection == "Paper" && computerSelection == "Scissors")) {
-        return "lose";
+        return `You Lose! ${computerSelection} beats ${playerSelection}.`;
     }
     else {
         return "invalid input";
     }
+
+
 }
+
+console.log(playMatch(window.prompt("Choose Rock, Paper, or Scissors", ),computerPlay()));
